d3195f4a6818b9b55efa1cd255a8d1f53e78086b
Fahim <fa9010583@gmail.com>
2023-05-04 13:18:42 +0600

    Update file.c

--- file.c
+++ file.c
@@ -7,10 +7,10 @@ int main()
     file1=fopen("read.txt","r");
     file2=fopen("write.txt","w");
     char str[100];
-    //fscanf(file1,"%[^\n]s",str);
-    fgets(str,100,file1);
-    //fprintf(file2,"%s",str);
-    fputs(str,file2);
+    fscanf(file1,"%[^\n]s",str);
+    //fgets(str,100,file1);
+    fprintf(file2,"%s",str);
+    //fputs(str,file2);
 
     fclose(file1);
     fclose(file2);
