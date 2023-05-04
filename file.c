8f8c2fed1e8dd9705efbdeb50ae088dde67e4a96
Fahim <fa9010583@gmail.com>
2023-05-04 13:17:53 +0600

    Update file.c

--- file.c
+++ file.c
@@ -1,16 +1,18 @@
+//file input/output
 #include<stdio.h>
 #include<stdlib.h>
 int main()
 {
-    FILE *file,*file2;
-    file=fopen("test.txt","w");
-    file2=fopen("read.txt","r");
-    char name[20];
-    fscanf(file2,"%[^\n]s",name);
-    //fgets(name,30,file2);///Input line from file
-    fprintf(file,"%s",name);
-   
-    //printf("%s\n",name);
-    fclose(file);
+    FILE *file1, *file2;
+    file1=fopen("read.txt","r");
+    file2=fopen("write.txt","w");
+    char str[100];
+    //fscanf(file1,"%[^\n]s",str);
+    fgets(str,100,file1);
+    //fprintf(file2,"%s",str);
+    fputs(str,file2);
+
+    fclose(file1);
     fclose(file2);
+
 }
